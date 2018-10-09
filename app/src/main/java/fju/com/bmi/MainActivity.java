3b421d834b32e1873329a9d60d21e217663c4e1f
hshken <hshken@livemail.tw>
2018-10-09 15:22:08 +0800

    BMI

--- app/src/main/java/fju/com/bmi/MainActivity.java
+++ app/src/main/java/fju/com/bmi/MainActivity.java
@@ -5,18 +5,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editWeight;
+    EditText editHeight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editWeight= findViewById(R.id.ed_Weight);
+        editHeight = findViewById(R.id.ed_Height);
+        //取得方塊內資料，轉成屬性
     }
     public void BMI(View view){
         Log.d("MainActivity","BMI");
-        EditText editWeight = findViewById(R.id.ed_Weight);
-        EditText editHeight = findViewById(R.id.ed_Height);
+        //取得輸入方塊
         String w = editWeight.getText().toString();
         String h = editHeight.getText().toString();
         Log.d("MainActivity",w +"/"+ h);
@@ -24,5 +28,6 @@ public class MainActivity extends AppCompatActivity {
         float height =Float.parseFloat(h);
         float BMI = weight/(height*height);
         Log.d("MainActivity",BMI+" ");
+        Toast.makeText(this, "Your BMI is " + BMI, Toast.LENGTH_LONG).show();
     }
 }
